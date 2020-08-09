3e3532bf369e026a678a9e99a0baf6444b77854a
tramvay5401 <tramvay5401@gmail.com>
2020-08-09 08:48:59 +0300

    Added Home work  number two

---
+++ MyHomeWork_2/index.js
@@ -0,0 +1,15 @@
+let admin
+let name = "Джон"
+admin = name
+console.log(admin)
+admin = "Юра"
+alert(admin)
+
+name = prompt('Как тебя звать ?','')
+alert(name)
+
+let num1, num2
+num1 = prompt('Введите число 1 ', '')
+num2 = prompt('Введите число 2 ', '')
+let result = Number(num1) + Number(num2)
+alert(result)
